2c4f269a89d4404ce804518d58004251574c8c47
glendc <glen.decauwsemaecker@otainsight.com>
2021-11-15 14:57:37 +0100

    reset streamer ticket if flushed, regardless of an error
    as there is no point in reflushing again if it already did,
    given the fact that by convention we drop rows when flushed, even if it failed

--- streamer.go
+++ streamer.go
@@ -252,7 +252,8 @@ func (s *Streamer) doWork(client bigquery.Client, maxBatchDelay time.Duration) {
 			flushed, err := client.Put(job.Data)
 			if err != nil {
 				s.logger.Errorf("worker thread data job received: put data to client: failure: %v", err)
-			} else if flushed {
+			}
+			if flushed {
 				batchDelayTicker.Reset(maxBatchDelay)
 				s.logger.Debug("worker thread data job received: put data to client: flushed all batched rows")
 			}
